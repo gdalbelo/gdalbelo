4b59e1b743ae7c9b000ad8e8f030b05608af009e
Gabriel Dal Belo <gab_belo@hotmail.com.br>
2014-05-05 18:47:31 +0000

    ok

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Garantiaextra::Application.config.secret_key_base = '6af6a5457c1e6bf6823c11912d379bc03b38369371e5d699fdd2ee60362ce3ee89596401e4b6f9d768890b7c2acd88fe8c20d0b51d7767424920eae57a6e66ff'
